215a5ae2e680c44cff7357a689cabb1b0cd57da9
sumartoyo <dimas.gs@outlook.com>
2016-01-31 14:20:11 +0700

    use __init__.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+    name = 'hasel',
+    packages = ['hasel'],
+    version = '1.0.0',
+    description = 'python+numpy RGB to HSL (and vice versa) converter',
+    author = 'Dimas Sumartoyo',
+    author_email = 'dimas.gs@outlook.com',
+    url = 'https://github.com/sumartoyo/hasel',
+    download_url = 'https://github.com/sumartoyo/hasel/tarball/1.0.0',
+    keywords = ['rgb', 'hsl', 'color', 'image'],
+)
